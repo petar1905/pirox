e32ee59fad3278dab9b8c65b2d77cdc194ae7ff6
ppsn <alhaitham@tuta.io>
2023-09-03 22:51:13 +0300

    wrapped calculate function in a pub module

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,26 @@
 #![allow(dead_code)]
 
-use string_traits::StringCalculate;
-use wasm_bindgen::prelude::wasm_bindgen;
+
 mod string_traits;
 mod char_traits;
 mod operation;
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub mod pirox {
+    use crate::string_traits::StringCalculate;
+    use wasm_bindgen::prelude::wasm_bindgen;
+    #[wasm_bindgen]
+    pub fn calculate(expression: String) -> i32 {
+        expression.calculate()
+    }
 }
+
 
 #[cfg(test)]
 mod tests {
     use crate::string_traits::StringCalculate;
-
     #[test]
     fn parse_parentheses() {
         let expr = "((3*(1+1))+((2*2)*2))/(3-1)".to_string();
         println!("{}", expr.calculate())
     }
-}
-
-#[wasm_bindgen]
-pub fn calculate(expression: String) -> i32 {
-    expression.calculate()
 }
